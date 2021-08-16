508d9e7cfb75c50a92943e959260db3daafa34f5
Sunspire <gfdavid@gmail.com>
2021-08-16 18:16:22 +0400

    wip

--- classes/command_controller.py
+++ classes/command_controller.py
@@ -4,8 +4,11 @@ import os
 
 class CommandController:
     def __init__(self):
-        self.market = ''
-
+        self.templates = {
+            'standard': 'standard_template.txt',
+            'openers': 'openers_template.txt',
+            'clickers': 'clickers_template.txt'
+        }
 
     def replace_placeholder(self, placeholder = {}, template = '', key = ''):
         generated_script = template
@@ -61,7 +64,53 @@ class CommandController:
 
 
     def generate_script(self, key: str, template_name: str, directory_name: str):
-        pass
+        try:
+            with open('templates/' + self.templates[template_name], 'r') as file_template:
+                template = file_template.read()
+
+        except KeyError:
+            print('Cannot find the template')
+            return
+
+        placeholder = {}
+        with open('configuration/template_config.json', 'r', encoding='utf-8') as f:
+            json_data = json.load(f)
+            try:
+                placeholder['table_name'] = str(json_data[key]['table_name'])
+                placeholder['btq_of_origin'] = ', '.join(map(lambda x: "'" + x.strip() + "'", str(json_data[key]['btq_of_origin']).split(',')))
+                placeholder['central_one_to_many'] = str(json_data[key]['central_one_to_many'])
+                placeholder['external_key'] = str(json_data[key]['external_key'])
+                placeholder['pool_language'] = "'" + str(json_data[key]['pool_language']) + "'"
+                placeholder['country'] = "'" + str(json_data[key]['country']) + "'"
+                placeholder['salutation'] = ''
+                
+            except KeyError:
+                print(f'Key: {key} not found')
+
+            language_file = f'configuration/language/{key}.txt'
+        if os.path.isfile(language_file):
+            with open(language_file) as f:
+                language = f.read()
+                placeholder['pool_language'] = language
+        
+        salutation_file = f'configuration/salutation/{key}.txt'
+        if os.path.isfile(salutation_file):
+            with open(salutation_file) as f:
+                salutation = f.read()
+                placeholder['salutation'] = salutation
+
+        generated_script = self.replace_placeholder(placeholder, template, key)
+        
+        directory_path = f'output/{directory_name}/'
+        if not os.path.isdir(directory_path):
+            os.makedirs(directory_path)
+
+        file_name_and_path = f'output/{directory_name}/standard_script_{key}.sql'
+        if os.path.isfile(file_name_and_path):
+            os.remove(file_name_and_path)
+
+        with open(f'output/{directory_name}/standard_script_{key}.sql', 'w') as f:
+            f.write(generated_script)
 
 
     def europe(self):
